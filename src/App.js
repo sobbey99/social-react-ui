65624b9d2965eece6a199b82852835bb99e87b46
sobbey99 <sobbey99@gmail.com>
2023-02-25 16:16:13 +0200

    Login Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
+import Login from "./pages/login/Login";
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <>
+      <Login/>
+    </>
   );
 }
 
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,26 @@
+import './login.scss'
+
+const Login = () => {
+  return (
+    <div className='login'>
+        <div className="card">
+            <div className="left">
+                <h1>Hello World</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quaerat ullam distinctio reiciendis dicta aut voluptatum ratione, eos dolorem iure magni necessitatibus facilis, voluptas soluta!</p>
+                <span>Don't you have an account?</span>
+                <button>Register</button>
+            </div>
+            <div className="right">
+                <h2>Login</h2>
+                <form>
+                    <input type="text" placeholder='Username' />
+                    <input type="password" placeholder='Password' />
+                    <button>Login</button>
+                </form>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Login
