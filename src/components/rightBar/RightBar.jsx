ccbe0a52835eb4c8b588ab8f6989344337243a8e
sobbey99 <sobbey99@gmail.com>
2023-02-25 17:45:46 +0200

    Leftbar and Rightbar

--- src/components/rightBar/RightBar.jsx
+++ src/components/rightBar/RightBar.jsx
@@ -2,7 +2,105 @@ import './RightBar.scss'
 
 const RightBar = () => {
   return (
-    <div className="rightBar">RightBar</div>
+    <div className="rightBar">
+      <div className="container">
+        <div className="item">
+          <span>Suggestions For You</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.unsplash.com/photo-1514913274516-4aa04f176f8c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1021&q=80" alt="" />
+              <span>Jane Doe</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.unsplash.com/photo-1514913274516-4aa04f176f8c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1021&q=80" alt="" />
+              <span>Jane Doe</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="item">
+          <span>Latest Activities</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.unsplash.com/photo-1514913274516-4aa04f176f8c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1021&q=80" alt="" />
+              <p>
+              <span>Jane Doe</span> changed their cover picture
+              </p>
+            </div>
+              <span>1 min ago</span>
+          </div>
+
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.unsplash.com/photo-1514913274516-4aa04f176f8c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1021&q=80" alt="" />
+              <p>
+              <span>Jane Doe</span> changed their cover picture
+              </p>
+            </div>
+              <span>1 min ago</span>
+          </div>
+
+
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.unsplash.com/photo-1514913274516-4aa04f176f8c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1021&q=80" alt="" />
+              <p>
+              <span>Jane Doe</span> changed their cover picture
+              </p>
+            </div>
+              <span>1 min ago</span>
+          </div>
+        </div>
+
+
+        <div className="item">
+          <span>Online Friends</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.unsplash.com/photo-1514913274516-4aa04f176f8c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1021&q=80" alt="" />
+              <div className='online'/>
+              <span>Jane Doe</span>
+            </div>
+          </div>
+
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.unsplash.com/photo-1514913274516-4aa04f176f8c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1021&q=80" alt="" />
+              <div className='online'/>
+              <span>Jane Doe</span>
+            </div>
+          </div>
+
+
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.unsplash.com/photo-1514913274516-4aa04f176f8c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1021&q=80" alt="" />
+              <div className='online'/>
+              <span>Jane Doe</span>
+            </div>
+          </div>
+
+
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.unsplash.com/photo-1514913274516-4aa04f176f8c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1021&q=80" alt="" />
+              <div className='online'/>
+              <span>Jane Doe</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
