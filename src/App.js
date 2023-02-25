10daa95efaf8917e0eccad03ef92a8d945b65507
sobbey99 <sobbey99@gmail.com>
2023-02-25 16:43:28 +0200

    ProtectedRoute

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import {
   createBrowserRouter,
   RouterProvider,
-  Outlet
+  Outlet,
+  Navigate
 } from "react-router-dom";
 
 import Login from "./pages/login/Login";
@@ -14,47 +15,63 @@ import LeftBar from "./components/leftBar/LeftBar";
 import RightBar from "./components/rightBar/RightBar";
 
 
-const Layout = () => {
-  return (
-    <div>
-      <Navbar />
-      <div style={{display: "flex"}}>
-      <LeftBar/>
-      <Outlet/>
-      <RightBar/>
-      </div>
-    </div>
-  )
-}
 
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout/>,
-    children: [
-      {
-        path: "/",
-        element: <Home />
-      },
-      {
-        path: "/profile/:id",
-        element: <Profile />
-      },
-    ]
-  },
-  {
-    path: "/login",
-    element: <Login/>,
-  },
-  {
-    path: "/register",
-    element: <Register/>,
-  },
-]);
 
 
 function App() {
+
+  const currentUser = true;
+
+  const Layout = () => {
+    return (
+      <div>
+        <Navbar />
+        <div style={{display: "flex"}}>
+        <LeftBar/>
+        <Outlet/>
+        <RightBar/>
+        </div>
+      </div>
+    )
+  }
+  
+
+  const ProtectedRoute = ({children}) => {
+    if(!currentUser) {
+      return <Navigate to="/login"/>
+    }
+
+    return children
+  }
+  
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <ProtectedRoute>
+          <Layout/>
+        </ProtectedRoute>
+      ),
+      children: [
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />
+        },
+      ]
+    },
+    {
+      path: "/login",
+      element: <Login/>,
+    },
+    {
+      path: "/register",
+      element: <Register/>,
+    },
+  ]);
   
   return (
     <div>
