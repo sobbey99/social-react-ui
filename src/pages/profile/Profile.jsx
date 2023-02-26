3342be5863bcbe3b17d10848a1039857766dc813
sobbey99 <sobbey99@gmail.com>
2023-02-26 16:51:43 +0200

    Profile Page

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,8 +1,65 @@
 import './profile.scss'
 
+import FacebookTwoToneIcon from '@mui/icons-material/FacebookTwoTone';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import PlaceIcon from '@mui/icons-material/Place';
+import LanguageIcon from '@mui/icons-material/Language';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+import Posts from '../../components/posts/Posts'
+
 const Profile = () => {
   return (
-    <div className='profile'>Profile</div>
+    <div className='profile'>
+      <div className="images">
+        <img src="https://plus.unsplash.com/premium_photo-1675198764206-40536cd9c362?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="cover" className="cover" />
+        <img src="https://images.unsplash.com/photo-1598550880863-4e8aa3d0edb4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80" alt="profilePic" className="profilePic" />
+      </div>
+      <div className="profileContainer">
+        <div className="uInfo">
+          <div className="left">
+            <a href="http://facebook.com">
+              <FacebookTwoToneIcon fontSize="large"/>
+            </a>
+            <a href="http://instagram.com">
+              <InstagramIcon fontSize="large"/>
+            </a>
+            <a href="http://twitter.com">
+              <TwitterIcon fontSize="large"/>
+            </a>
+            <a href="http://linkedin.com">
+              <LinkedInIcon fontSize="large"/>
+            </a>
+            <a href="http://pinterest.com">
+              <PinterestIcon fontSize="large"/>
+            </a>
+          </div>
+          <div className="center">
+            <span>Jane Doe</span>
+            <div className="infoProfile">
+              <div className="item">
+                <PlaceIcon />
+                <span>USA</span>
+              </div>
+              <div className="item">
+                <LanguageIcon />
+                <span>qqq.md</span>
+              </div>
+            </div>
+            <button>follow</button>
+          </div>
+          <div className="right">
+            <EmailOutlinedIcon />
+            <MoreVertIcon />
+          </div>
+        </div>
+        <Posts />
+      </div>
+    </div>
   )
 }
 
